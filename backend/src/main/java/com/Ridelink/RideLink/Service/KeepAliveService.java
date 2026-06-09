a3226421a12dfb51fa7b30434e9d5a7d0238b66d
dparmar72 <parmardipanshu876@gmail.com>
2026-06-09 13:08:32 +0530

    Update some security features and added health check

--- backend/src/main/java/com/Ridelink/RideLink/Service/KeepAliveService.java
+++ backend/src/main/java/com/Ridelink/RideLink/Service/KeepAliveService.java
@@ -4,6 +4,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+@Service
 public class KeepAliveService {
 
     private static final Logger logger = LoggerFactory.getLogger(KeepAliveService.class);
